e85907d0c48f749e22c9e71edc26b9c06124d250
demapuspita <demapuspita@gmail.com>
2023-01-04 20:42:27 +0700

    fix changes

--- add.php
+++ add.php
@@ -6,6 +6,10 @@ if(isset($_POST['submit'])){
 	$u_card = $_POST['card_no'];
 	$u_f_name = $_POST['user_first_name'];
 	$u_l_name = $_POST['user_last_name'];
+	$u_prodi = $_POST['user_prodi'];
+	$u_smt = $_POST['user_smt'];
+	$u_kelas = $_POST['user_kelas'];
+	$u_angkatan = $_POST['user_angkatan'];
 	$u_father = $_POST['user_father'];
 	$u_birthday = $_POST['user_dob'];
 	$u_gender = $_POST['user_gender'];
@@ -16,7 +20,6 @@ if(isset($_POST['submit'])){
 	$u_village = $_POST['village'];
 	$u_pincode = $_POST['pincode'];
 	$u_mother = $_POST['user_mother'];
-	$u_staff_id = $_POST['staff_id'];
 	
 
 
@@ -32,12 +35,9 @@ if(isset($_POST['submit'])){
   		$msg = "Gagal mengunggah foto";
   	}
 
-  	$insert_data = "INSERT INTO student_data(u_card, u_f_name, u_l_name, 
-	u_father, u_aadhar, u_birthday, u_gender, u_email, u_phone, u_state, 
-	u_dist, u_village, u_police, u_pincode, u_mother, u_family, staff_id,image,uploaded) 
-	VALUES ('$u_card','$u_f_name','$u_l_name','$u_father','$u_birthday',
-	'$u_gender','$u_email','$u_phone','$u_state','$u_dist','$u_village',
-	'$u_pincode','$u_mother','$u_staff_id','$image',NOW())";
+  	$insert_data = "INSERT INTO student_data(u_card, u_f_name, u_l_name, u_prodi, u_smt, u_kelas, u_angkatan, u_father, u_aadhar, u_birthday, u_gender, u_email, u_phone, u_state, 
+	u_dist, u_village, u_police, u_pincode, u_mother,image,uploaded) 
+	VALUES ('$u_card','$u_f_name','$u_l_name','$u_father','$u_birthday', '$u_gender','$u_email','$u_phone','$u_state','$u_dist','$u_village','$u_pincode','$u_mother','$image',NOW())";
   	$run_data = mysqli_query($con,$insert_data);
 
   	if($run_data){
